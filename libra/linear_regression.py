7feb5807b0f3f52a7bb0396fd33a9af3df721106
chrisistrati <chris.istrati@gmail.com>
2024-10-27 19:02:56 +0200

    im sad

--- libra/linear_regression.py
+++ libra/linear_regression.py
@@ -1,71 +1,36 @@
 import numpy as np
 import pandas as pd
-import math
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder
+import matplotlib.pyplot as plt
+import numpy as np
+ 
+class SLinearregression:
 
-class LinearRegressionSMLH():
-    #Define class to store functions
+    def __init__(self, learning_rate=0.0001, n_iters=1000):
+        self.lr = learning_rate
+        self.n_iters = n_iters
+        self.weights = None
+        self.bias = None
 
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
 
-    def __init__(self):
-        self.params__=None
-        """
-        Initializes the linear regression model
         
-        Stores the self.params__ variable which are the weights that the model returns
-        """
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        for _ in range(self.n_iters):
+            y_predicted = np.dot(X, self.weights) + self.bias
+           
+            dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y))
+            db = (1 / n_samples) * np.sum(y_predicted - y)
+
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
 
 
-    def gradientDescent(self, X:np.ndarray, y:np.ndarray, learning_rate=0.00001,
-        iterations=500, batch_size=16):
-
-        """This function applies the Gradient Descent model to the dataset
-        param X: numpy.ndarray
-            The X matrix containing the independent variable columns.
-        param y: numpy.ndarray
-            The target vector y."""
-        
-        #Add a column of ones for the constant term
-
-        y=y.to_numpy().reshape(-1, 1)
-        # X = np.concatenate([X, np.ones_like(y)], axis = 1)
-        rows, cols = X.shape
-
-        #Combine the X and y columns to more easily shuffle it later
-        X = np.append(X, y, axis=1)
-
-        #Make the initial random guess for w
-        w = np.random.random((cols, 1))
-
-        #Go through all the iterations
-        for i in range(iterations):
-
-            #Shuffle the rows of the data
-            np.random.shuffle(X)
-
-            #Define X and y again
-            y_it = X[:, -1].reshape((rows, 1))
-            X_it = X[:, :-1]
-
-            #Iterate through the batches
-            for batch in range(math.ceil(rows / batch_size)):
-
-                batch_start = batch * batch_size
-                
-                #Cut a batch from the dataset
-                x_batch = X_it[batch_start : min(batch_start + batch_size, X.shape[0])]
-                y_batch = y_it[batch_start : min(batch_start + batch_size, X.shape[0])]
-
-
-                #Subtract the gradient from our previous estimation
-                w -= learning_rate * np.matmul(x_batch.transpose(), (np.matmul(x_batch, w) - y_batch))
-
-        self.params__= w
-        return self
-    
-    
-    def predict(self, X:np.ndarray):
-        #predict given the data X
-        X = np.concatenate([X, np.ones(X.shape[0]).reshape
-        ((X.shape[0], 1))], axis=1)
-
-        return np.matmul(X, self.params__)
+    def predict(self, X):
+        y_approximated = np.dot(X, self.weights) + self.bias
+        return y_approximated
